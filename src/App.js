042249274e51dffdd49ea5953c73b0d196347414
IamGrootie <ligiatmonteiro@gmail.com>
2023-07-13 20:14:23 +0100

    navbar dev&style

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React from 'react'
 
 import { Article, Brand, CTA, Feature, Navbar } from './components';
 import { Blog, Possibility, Features, Footer, Header, WhatGPT3 } from './containers';
+import './styles.scss';
 
 function App() {
   return (
     <div className='App'>
       <div className='gradient-bg'>
-
+        <Navbar />
+        <Header />
       </div>
-
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { VscClose, VscListSelection } from 'react-icons/vsc'
 import '../styles.scss';
 
-function Navbar() {
-  return (
-    <div>
+import logo from '../assets/logo.svg';
 
+const Menu = () => (
+  <>
+    <p><a href="#home">Home</a></p>
+    <p><a href="#wgpt">What is GPT3?</a></p>
+    <p><a href="#possibility">Open AI</a></p>
+    <p><a href="#features">Case Studies</a></p>
+    <p><a href="#blog">Library</a></p>
+  </>
+)
+
+function Navbar() {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className='navbar'>
+      <div className='navbar-links'>
+        <div className='navbar-links-logo'>
+          <img src={logo} alt="" /> 
+        </div>
+        <div className='navbar-links-container'>
+          <Menu />
+        </div>
+      </div>
+      <div className='navbar-sign'>
+        <p>Sign in</p>
+        <button type='button'>Sign up</button>
+      </div>
+      <div className='navbar-menu'>
+        {toggleMenu 
+          ? <VscClose color='#fff' size={27} onClick={()=> setToggleMenu(false)} />
+          : <VscListSelection color='#fff' size={27} onClick={()=> setToggleMenu(true)} />
+        }
+        { toggleMenu && (
+          <div className='navbar-menu-container'> 
+            <div className='navbar-menu-container-links'>
+              <Menu />
+            </div>
+            <div className='navbar-menu-container-sign'>
+              <p>Sign in</p>
+              <button type='button'>Sign up</button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
