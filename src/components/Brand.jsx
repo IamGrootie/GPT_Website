50f22258b18a36286e20a7065d80efb8d35ec4a9
IamGrootie <ligiatmonteiro@gmail.com>
2023-07-18 17:32:20 +0100

    brand dev&style

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import '../styles.scss';
 
+import google from '../assets/google.png';
+import dropbox from '../assets/dropbox.png';
+import slack from '../assets/slack.png';
+import shopify from '../assets/shopify.png';
+import atlassian from '../assets/atlassian.png';
+
 function Brand() {
   return (
-    <div>
-        
+    <div className='brand section-padding'> 
+      <div>
+        <img src={google} alt="google logo"/>
+      </div>
+      <div>
+        <img src={slack} alt="slack logo"/>
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian logo"/>
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox logo"/>
+      </div>
+      <div>
+        <img src={shopify} alt="shopify logo"/>
+      </div>
     </div>
   )
 }
